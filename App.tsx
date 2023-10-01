a435f2a79b72cd4200b738390689e381fdb2041c
Raymond Li <hi@raymond.li>
2023-10-02 01:26:37 +0200

    Extract data out, use state and search

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Image,
+  ImageProps,
   Pressable,
   SafeAreaView,
   ScrollView,
@@ -32,16 +33,157 @@ const H3 = (props: { children: any }) => {
 };
 
 const Inft2508App = () => {
+  const images = {
+    logo: require('./assets/logo.png'),
+    resultPlaceholder: require('./assets/result-placeholder.png'),
+    newsPlaceholder: require('./assets/news-placeholder.png'),
+  };
+
+  const initialResultsToday = [
+    {
+      id: 1,
+      img: images.resultPlaceholder,
+      highlight: 'Jackpot Winner',
+      label: 'Norsk Tipping',
+      numbers: '1 2 3 4 5',
+    },
+    {
+      id: 2,
+      img: images.resultPlaceholder,
+      highlight: '2nd Prize Winner',
+      label: 'Powerball',
+      numbers: '6 7 8 9 10',
+    },
+    {
+      id: 3,
+      img: images.resultPlaceholder,
+      highlight: '3rd Prize Winner',
+      label: 'Mega Millions',
+      numbers: '11 12 13 14 15',
+    },
+    {
+      id: 4,
+      img: images.resultPlaceholder,
+      label: 'EuroMillions',
+      numbers: '12 13 14 15 16 17',
+    },
+  ];
+  const initialResultsPrevious = [
+    {
+      id: 1,
+      img: images.resultPlaceholder,
+      highlight: '5th Prize Winner',
+      label: 'Lotto 6/49',
+      numbers: '1 2 3 4 5',
+    },
+    {
+      id: 2,
+      img: images.resultPlaceholder,
+      highlight: '8th Prize Winner',
+      label: 'SuperEnalotto',
+      numbers: '6 7 8 9 10',
+    },
+    {
+      id: 3,
+      img: images.resultPlaceholder,
+      highlight: '7th Prize Winner',
+      label: 'Mega Millions',
+      numbers: '11 12 13 14 15',
+    },
+    {
+      id: 4,
+      img: images.resultPlaceholder,
+      label: 'EuroMillions',
+      numbers: '12 13 14 15 16 17',
+    },
+  ];
+
+  const initialNews = [
+    {
+      id: 1,
+      img: images.newsPlaceholder,
+      title: 'New Lottery Game Launching Soon',
+      date: 'October 1, 2023',
+      source: 'LotteryNEWS',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci eget fauci...',
+    },
+    {
+      id: 2,
+      img: images.newsPlaceholder,
+      title: 'Tips to Increase Your Chances of Winning',
+      date: 'October 1, 2023',
+      source: 'Norsk Tipping',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci eget fauci...',
+    },
+  ];
+
+  // Initial state
+  const [resultsToday, setResultsToday] = useState(initialResultsToday);
+  const [resultsPrevious, setResultsPrevious] = useState(
+    initialResultsPrevious,
+  );
+  const [news, setNews] = useState(initialNews);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [dateToday, setDateToday] = useState(
+    new Date(Date.now()).toLocaleDateString(),
+  );
+
+  const searchChangedHandler = (search: string) => {
+    const matchedResultsToday: {
+      id: number;
+      img: ImageProps;
+      label: string;
+      numbers: string;
+      highlight?: string;
+    }[] = [];
+    const matchedResultsPrevious: {
+      id: number;
+      img: ImageProps;
+      label: string;
+      numbers: string;
+      highlight?: string;
+    }[] = [];
+    const matchedNews: {
+      id: number;
+      img: ImageProps;
+      title: string;
+      date: string;
+      source: string;
+      text: string;
+    }[] = [];
+    if (search.trim().length > 0) {
+      resultsToday.map(value => {
+        if (value.numbers.includes(search)) {
+          matchedResultsToday.push(value);
+        }
+      });
+      resultsPrevious.map(value => {
+        if (value.numbers.includes(search)) {
+          matchedResultsPrevious.push(value);
+        }
+      });
+      news.map(value => {
+        if (value.title.includes(search)) {
+          matchedNews.push(value);
+        }
+      });
+      setResultsToday(matchedResultsToday);
+      setResultsPrevious(matchedResultsPrevious);
+      setNews(matchedNews);
+    } else {
+      setResultsToday(initialResultsToday);
+      setResultsPrevious(initialResultsPrevious);
+      setNews(initialNews);
+    }
+  };
+
   return (
     <SafeAreaView style={Styles.screen.container}>
       {/*  Fixed header */}
       <View style={Styles.header.container}>
         <MaterialIcons name="arrow-back-ios-new" size={24} color="grey" />
         <Text style={Styles.header.title}>Lottery App</Text>
-        <Image
-          source={require('./assets/logo.png')}
-          style={Styles.header.icon}
-        />
+        <Image source={images.logo} style={Styles.header.icon} />
       </View>
       {/* Scrollable page  */}
       <ScrollView style={Styles.page.container}>
@@ -51,6 +193,7 @@ const Inft2508App = () => {
             style={Styles.search.input}
             placeholder="Search for lottery numbers"
             placeholderTextColor={'#888'}
+            onChangeText={searchChangedHandler}
           />
           <Pressable style={Styles.search.button}>
             <MaterialIcons name="search" size={24} color="grey" />
@@ -60,32 +203,22 @@ const Inft2508App = () => {
         <Section>
           <SectionHeading>
             <H1>Today's Lottery Results</H1>
-            <H3>October 1, 2023</H3>
+            <H3>{dateToday}</H3>
           </SectionHeading>
           <ScrollView horizontal={true}>
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'Jackpot Winner'}
-              label={'Norsk Tipping'}
-              numbers={'1 2 3 4 5'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'2nd Prize Winner'}
-              label={'Powerball'}
-              numbers={'6 7 8 9 10'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'3rd Prize Winner'}
-              label={'Mega Millions'}
-              numbers={'11 12 13 14 15'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              label={'EuroMillions'}
-              numbers={'12 13 14 15 16 17'}
-            />
+            {resultsToday.length > 0 ? (
+              resultsToday.map(result => (
+                <LotteryCard
+                  key={result.id}
+                  img={result.img}
+                  highlight={result.highlight}
+                  label={result.label}
+                  numbers={result.numbers}
+                />
+              ))
+            ) : (
+              <H3>No results found</H3>
+            )}
           </ScrollView>
         </Section>
         {/*  Previous lottery results */}
@@ -95,29 +228,19 @@ const Inft2508App = () => {
             <SeeMore text={'View All Results'} />
           </SectionHeading>
           <ScrollView horizontal={true}>
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'5th Prize Winner'}
-              label={'Lotto 6/49'}
-              numbers={'1 2 3 4 5'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'8th Prize Winner'}
-              label={'SuperEnalotto'}
-              numbers={'6 7 8 9 10'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              highlight={'7th Prize Winner'}
-              label={'Mega Millions'}
-              numbers={'11 12 13 14 15'}
-            />
-            <LotteryCard
-              img={require('./assets/result-placeholder.png')}
-              label={'EuroMillions'}
-              numbers={'12 13 14 15 16 17'}
-            />
+            {resultsPrevious.length > 0 ? (
+              resultsPrevious.map(result => (
+                <LotteryCard
+                  key={result.id}
+                  img={result.img}
+                  highlight={result.highlight}
+                  label={result.label}
+                  numbers={result.numbers}
+                />
+              ))
+            ) : (
+              <H3>No results found</H3>
+            )}
           </ScrollView>
         </Section>
         {/*  Lottery news */}
@@ -127,24 +250,20 @@ const Inft2508App = () => {
             <SeeMore text={'View All News'} />
           </SectionHeading>
           <View style={Styles.news.container}>
-            <NewsItem
-              img={require('./assets/news-placeholder.png')}
-              title={'New Lottery Game Launching Soon'}
-              date={'October 1, 2023'}
-              source={'LotteryNEWS'}
-              text={
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci eget fauci...'
-              }
-            />
-            <NewsItem
-              img={require('./assets/news-placeholder.png')}
-              title={'Tips to Increase Your Chances of Winning'}
-              date={'October 1, 2023'}
-              source={'Norsk Tipping'}
-              text={
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed orci eget fauci...'
-              }
-            />
+            {news.length > 0 ? (
+              news.map(item => (
+                <NewsItem
+                  key={item.id}
+                  img={item.img}
+                  title={item.title}
+                  date={item.date}
+                  source={item.source}
+                  text={item.text}
+                />
+              ))
+            ) : (
+              <H3>No news found</H3>
+            )}
           </View>
         </Section>
       </ScrollView>
